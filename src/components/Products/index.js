5d30552fde0a38c16ea082f8d42ad01f7d4d63d3
raja <rajababu095221@gmail.com>
2024-01-25 13:09:49 +0000

    nxttrendz authentication2

---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-route">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="products-sec-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
